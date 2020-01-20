ac8c221fc6e207386a5c5e336579a91a4a01e2a7
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-20 14:42:42 +0100

    ManyToMany

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,13 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Course {
@@ -13,6 +17,9 @@ public class Course {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
+    
     public Course() {
     }
 
@@ -35,4 +42,12 @@ public class Course {
     public void setName(String name) {
         this.name = name;
     }
+
+	public List<Wizard> getWizards() {
+		return wizards;
+	}
+
+	public void setWizards(List<Wizard> wizards) {
+		this.wizards = wizards;
+	}
 }
